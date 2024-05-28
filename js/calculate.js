a200230e26ee18b27195e004259b52785896bf33
Tatyana <40871489+tatyanka-study@users.noreply.github.com>
2024-05-28 16:13:46 +0300

    First commit

---
+++ js/calculate.js
@@ -0,0 +1,12 @@
+const operators = {
+    sum: "+",
+    subt: "-",
+    multi: "*",
+    divis: "/",
+}
+
+function calculate (a, b, operators) {
+    switch operators:
+    case operators.sum:
+        
+}
---
+++ js/mathfunctions.js
@@ -0,0 +1,19 @@
+function sum (a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtraction (a, b) {
+    let subtraction = a - b;
+    return subtraction; 
+}
+
+function multiplication (a, b) {
+    let multiplication = a - b;
+    return multiplication; 
+}
+
+function division (a, b) {
+    let division = a - b;
+    return division; 
+}
